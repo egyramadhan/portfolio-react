a32a86c2c93635995820e780974228d47b613103
rainflore <egyramadhan3@gmail.com>
2022-03-27 22:00:44 +0700

    nav, home, social media

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
